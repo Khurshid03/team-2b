2832d34af233eb31969efc4ae23414669a097558
Khurshid Ergashev <khurshidergashev@Khurshids-MacBook-Pro.local>
2025-05-12 11:29:31 -0400

    Testing

---
+++ astudio/app/src/test/java/com/example/astudio/model/ReviewManagerTest.java
@@ -0,0 +1,20 @@
+package com.example.astudio.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReviewManagerTest {
+
+    @Test
+    public void postReview() {
+    }
+
+    @Test
+    public void updateReview() {
+    }
+
+    @Test
+    public void deleteReview() {
+    }
+}
